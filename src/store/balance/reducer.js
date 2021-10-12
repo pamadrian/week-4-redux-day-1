193f128b983af2e92df0bc6088721613c919903a
pamadrian <pamvanalfen@icloud.com>
2021-10-12 11:56:11 +0200

    created first reducer

---
+++ src/store/balance/reducer.js
@@ -0,0 +1,12 @@
+// src/store/balance/reducer.js
+const initialState = {
+  amount: 0,
+};
+
+export default function reducer(state = initialState, action) {
+  switch (action.type) {
+    default: {
+      return state;
+    }
+  }
+}
