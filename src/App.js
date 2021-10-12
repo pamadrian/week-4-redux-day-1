7f6952954648e6b95341f5f27aa9033098ed4645
pamadrian <pamvanalfen@icloud.com>
2021-10-12 12:07:16 +0200

    imported usedispatch into app.js and dispatched the action

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { deposit } from "./store/balance/actions";
 
 function App() {
+  const dispatch = useDispatch();
   const [balance, setBalance] = useState(0);
 
   return (
@@ -8,7 +11,7 @@ function App() {
       <p>Balance: {balance}$</p>
       <button
         onClick={() => {
-          setBalance(balance + 10);
+          dispatch(deposit(10));
         }}
       >
         Deposit 10$
