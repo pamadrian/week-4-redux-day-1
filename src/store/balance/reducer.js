d083e822ee8c3821efd3955c8ef81d4ce8772f47
pamadrian <pamvanalfen@icloud.com>
2021-10-12 12:13:47 +0200

    added a case to reducer

--- src/store/balance/reducer.js
+++ src/store/balance/reducer.js
@@ -5,6 +5,12 @@ const initialState = {
 
 export default function reducer(state = initialState, action) {
   switch (action.type) {
+    case "balance/deposit": {
+      return {
+        ...state,
+        amount: state.amount + action.payload,
+      };
+    }
     default: {
       return state;
     }
