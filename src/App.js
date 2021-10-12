3c12bc58a187a3a7bec155389fb441f96a46adcd
pamadrian <pamvanalfen@icloud.com>
2021-10-12 12:19:51 +0200

    used the selector in a component. by importing the useSelector hook. it allows to replace the useState call by assigning useSelector(selecBalance) to balance/

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import { useState } from "react";
-import { useDispatch } from "react-redux";
+// import { useState } from "react";
+// import { useDispatch } from "react-redux";
 import { deposit } from "./store/balance/actions";
+import { useDispatch, useSelector } from "react-redux";
+import { selectBalance } from "./store/balance/selectors";
 
 function App() {
   const dispatch = useDispatch();
-  const [balance, setBalance] = useState(0);
+  // const [balance, setBalance] = useState(0);
+  const balance = useSelector(selectBalance);
 
   return (
     <div className="App">
