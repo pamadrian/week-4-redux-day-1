903cda9f6e3a1a914f1834e95ee9808e877e474a
pamadrian <pamvanalfen@icloud.com>
2021-10-12 11:53:56 +0200

    configured react-redux, created redux store

---
+++ src/store/index.js
@@ -0,0 +1,17 @@
+// src/store/index.js
+
+import { combineReducers, createStore } from "redux";
+import balanceReducer from "./balance/reducer";
+
+const enhancer = window.__REDUX_DEVTOOLS_EXTENSION__
+  ? window.__REDUX_DEVTOOLS_EXTENSION__()
+  : (x) => x;
+
+const store = createStore(
+  combineReducers({
+    balance: balanceReducer,
+  }),
+  enhancer
+);
+
+export default store;
